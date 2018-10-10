c1eab0f724f3b60d5a4be587336486a10ef3d4f5
nickna <nickna@augmentum.com.cn>
2018-10-10 09:24:06 +0800

    refine config

--- src/config_list.py
+++ src/config_list.py
@@ -2,6 +2,13 @@ configs = {
     'baidu-home': 'baidu/home.json',
 }
 
+# default 127.0.0.1:8000
 configs.update({
-    'local-login': ('ajinga/login.json', 'local-data'),
+    'login': ('ajinga/login.json', 'default/login'),
+    'hr': ('ajinga/login.json', 'default/login-hr'),
+})
+
+# local 127.0.0.1:8000
+configs.update({
+    'local-login': ('ajinga/login.json', 'local/login'),
 })
