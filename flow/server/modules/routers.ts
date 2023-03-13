418c10ebe0cddd09943076aac806ced00da68349
那光情 <naguangqing@zhongan.com>
2023-03-13 12:53:47 +0800

    feat: modify

--- flow/server/modules/routers.ts
+++ flow/server/modules/routers.ts
@@ -3,13 +3,13 @@ import { v4 as uuidv4 } from 'uuid';
 
 import Router from 'koa-router';
 import koaBody from 'koa-body';
-import { hello, helloSave } from './hello';
+// import { hello, helloSave } from './hello';
 import { genMembers } from './fake';
 
 const router = new Router();
 
-router.get('/api/hello', koaBody(), hello);
-router.get('/api/hello/save', koaBody(), helloSave);
+// router.get('/api/hello', koaBody(), hello);
+// router.get('/api/hello/save', koaBody(), helloSave);
 
 router.get('/api/fake/genMembers', koaBody(), (ctx) => {
   ctx.body = {
